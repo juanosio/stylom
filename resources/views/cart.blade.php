9a9f82b05ca2c8ee551f156b0631d08bd27639de
juanosio <juanosio14@gmail.com>
2019-12-06 20:49:31 -0400

    Listando productos en el carrito de compra

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,18 +1,11 @@
 @extends("partials.web.layout")
 
 
-@section('content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-
-				
-		
+@section('content')		
 	<!-- Title Page -->
 	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image:  url({{ asset('assets/web/images/heading-pages-01.jpg') }}); ">
 		<h2 class="l-text2 t-center">
-			Cart
+			Carrito de compra
 		</h2>
 	</section>
 
@@ -22,64 +15,24 @@
 			<!-- Cart item -->
 			<div class="container-table-cart pos-relative">
 				<div class="wrap-table-shopping-cart bgwhite">
-					<table class="table-shopping-cart">
-						<tr class="table-head">
-							<th class="column-1"></th>
-							<th class="column-2">Producto</th>
-							<th class="column-3">Precio</th>
-							<th class="column-4 p-l-70">Cantidad</th>
-							<th class="column-5">Total</th>
-						</tr>
+				<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Producto</th>
+      <th scope="col">Precio</th>
 
-						<tr class="table-row">
-							<td class="column-1">
-								<div class="cart-img-product b-rad-4 o-f-hidden">
-									<img src="images/item-10.jpg" alt="IMG-PRODUCT">
-								</div>
-							</td>
-							<td class="column-2">Men Tshirt</td>
-							<td class="column-3">$36.00</td>
-							<td class="column-4">
-								<div class="flex-w bo5 of-hidden w-size17">
-									<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-									</button>
-
-									<input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
-
-									<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-									</button>
-								</div>
-							</td>
-							<td class="column-5">$36.00</td>
-						</tr>
-
-					
-					</table>
+    </tr>
+  </thead>
+  
+  <tbody id="carroMuestra">
+	<!-- Aqui se pinta todos los productos dinamicamente -->
+  </tbody>
+</table>
 				</div>
 			</div>
 
-			<div class="flex-w flex-sb-m p-t-25 p-b-25 bo8 p-l-35 p-r-60 p-lr-15-sm">
-				<div class="flex-w flex-m w-full-sm">
-					<div class="size11 bo4 m-r-10">
-						<input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="coupon-code" placeholder="Coupon Code">
-					</div>
 
-					<div class="size12 trans-0-4 m-t-10 m-b-10 m-r-10">
-						<!-- Button -->
-						<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-							Aplicar cupon de descuento
-						</button>
-					</div>
-				</div>
-
-				<div class="size10 trans-0-4 m-t-10 m-b-10">
-					<!-- Button -->
-					<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-						Actualizar carrito
-					</button>
-				</div>
 			</div>
 
 			<!-- Total -->
@@ -159,9 +112,54 @@
 		</div>
 	</section>
 
+	<script>
+ const PintarCarrito = () => {
+
+//Asigno el id a div que es donde voy a pintar toda la info
+const carroMuestra = document.getElementById('carroMuestra');
+carroMuestra.innerHTML = '';
+
+// Busco la informacion del carrito 
+listaArticulos = JSON.parse(localStorage.getItem('carrito'));
 
 
-</body>
-</html>
+if (listaArticulos === null) {
+	listaArticulos = [];
+	
+	//Aqui deberia mostrar un mensaje de que no hay nada en el carrito
+	// y un boton para que busque
+	
+	
+}else{
+	var total = 0;
+	listaArticulos.forEach(element => {
+
+	//Pinto lo que esta en el localStorage en el carrito
+	carroMuestra.innerHTML += `
+  <tr class="table-row">
+		<td class="column-1">
+			<div class="cart-img-product b-rad-4 o-f-hidden">
+				<img src="${element.fotoV}" alt="IMG-PRODUCT">
+			</div>
+		</td>
+		<td class="column-2">${element.nombre}</td>
+		<td class="column-3">${element.precio}</td>
+		
+	</tr>
+  `
+
+  //Cuando hagas lo que te dije de guardar el precio como un numero
+  //Entonces esto funcionara
+total += element.precio;
+});
+console.log(total);
+
+} //Cierre del if
+
+ } //Cierre de funcion
+
+document.addEventListener('DOMContentLoaded', PintarCarrito);
+
+</script>
 
 @endsection
